f34c948235efa24b55234cabcc35b88328912b71
Michael Fulton <fulto081@umn.edu>
2018-12-24 23:38:01 -0600

    Added -y's and remove b-e from ROS stuff.

--- install_software.bash
+++ install_software.bash
@@ -46,10 +46,10 @@ echo -e "\n${green}INSTALLING ROS ...${reset}"
 sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
 sudo apt-key adv --keyserver hkp://ha.pool.sks-keyservers.net:80 --recv-key 421C365BD9FF1F717815A3895523BAEEB01FA116
 sudo apt update
-sudo apt install ros-melodic-desktop-full
+sudo apt install ros-melodic-desktop-full -y
 sudo rosdep init
 rosdep update
 
 echo "source /opt/ros/melodic/setup.bash" >> ~/.bashrc
-sudo apt install python-rosinstall python-rosinstall-generator python-wstool build-essential
+sudo apt install python-rosinstall python-rosinstall-generator python-wstool -y
 echo -e "\n${green}DONE WITH ROS!${reset}"
