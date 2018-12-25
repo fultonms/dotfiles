7d4fd7491ead7f9e3414887cafdda0f90cd3d488
Michael Fulton <fulto081@umn.edu>
2018-12-24 22:59:35 -0600

    Added ROS and build-essential to install list.

--- install_software.bash
+++ install_software.bash
@@ -32,9 +32,24 @@ sudo snap install spotify
 # Install development software
 echo -e "${cyan}-----------------INSTALLING DEVELOPMENT SOFTWARE-------------------${reset}"
 
+echo -e "\n${green}INSTALLING BUILD-ESSENTIAL ...${reset}"
+sudo apt install build-essential -y
+
 echo -e "\n${green}INSTALLING VSCODE${reset}"
 curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg
 sudo mv microsoft.gpg /etc/apt/trusted.gpg.d/microsoft.gpg
 sudo sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list'
 sudo apt-get update
 sudo apt-get install code -y
+
+echo -e "\n${green}INSTALLING ROS ...${reset}"
+sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
+sudo apt-key adv --keyserver hkp://ha.pool.sks-keyservers.net:80 --recv-key 421C365BD9FF1F717815A3895523BAEEB01FA116
+sudo apt update
+sudo apt install ros-melodic-desktop-full
+sudo rosdep init
+rosdep update
+
+echo "source /opt/ros/melodic/setup.bash" >> ~/.bashrc
+sudo apt install python-rosinstall python-rosinstall-generator python-wstool build-essential
+echo -e "\n${green}DONE WITH ROS!${reset}"
